4d4ab21f6cd8060c1c6357ebe83f4440a8100cc4
Nicolas Becharat <nicolas.becharat@gmail.com>
2024-04-07 23:52:32 +0200

    add dashboard userid

--- backend/controllers/profilController.js
+++ backend/controllers/profilController.js
@@ -1,4 +1,4 @@
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
 export async function getProfil(req, res) {
@@ -8,15 +8,15 @@ export async function getProfil(req, res) {
       // Si l'id n'est pas défini, l'utilisateur n'est pas connecté
       return res.redirect("dashboard");
     }
-    
+
     const userInfo = await prisma.user.findUnique({
       where: {
-        id: userId
-      }, 
+        id: userId,
+      },
       select: {
         username: true,
-        lastDraw: true
-      }
+        lastDraw: true,
+      },
     });
 
     // console.log(userInfo);
@@ -27,8 +27,8 @@ export async function getProfil(req, res) {
 
     const numberCards = await prisma.userCard.count({
       where: {
-        id_user: userId
-      }
+        id_user: userId,
+      },
     });
     // console.log(numberCards, 'numberCards');
 
@@ -41,7 +41,7 @@ export async function getProfil(req, res) {
         card: true,
       },
     });
-    
+
     // console.log(cards_user, 'cards_user');
 
     let message = ""; // Initialise le message si l'utilisateur n'a pas de cartes
@@ -50,35 +50,38 @@ export async function getProfil(req, res) {
       // Si l'utilisateur n'a pas de cartes, affiche un message
       message = "Vous n'avez pas encore de cartes";
     }
-    console.log(message); 
+    console.log(message);
 
     if (userInfo.lastDraw !== null) {
-
       const lastDrawBigInt = BigInt(userInfo.lastDraw); // Convertir la valeur de date en BigInt
       const currentTimeBigInt = BigInt(new Date().getTime()); // Convertir le temps actuel en BigInt
-      const timeLeftBigInt = lastDrawBigInt + BigInt(24 * 60 * 60 * 1000) - currentTimeBigInt; // Calculer le temps restant en BigInt
+      const timeLeftBigInt =
+        lastDrawBigInt + BigInt(24 * 60 * 60 * 1000) - currentTimeBigInt; // Calculer le temps restant en BigInt
       const timeLeft = Number(timeLeftBigInt); // Convertir le temps restant en nombre entier
-      const hoursLeft = timeLeft > 0 ? Math.floor(timeLeft / (60 * 60 * 1000)) : 0; // Calculer les heures restantes
-      const minutesLeft = timeLeft > 0 ? Math.floor((timeLeft % (60 * 60 * 1000)) / (60 * 1000)) : 0; // Calculer les minutes restantes
-      
-      
+      const hoursLeft =
+        timeLeft > 0 ? Math.floor(timeLeft / (60 * 60 * 1000)) : 0; // Calculer les heures restantes
+      const minutesLeft =
+        timeLeft > 0
+          ? Math.floor((timeLeft % (60 * 60 * 1000)) / (60 * 1000))
+          : 0; // Calculer les minutes restantes
 
-      if (hoursLeft <= 0 || minutesLeft <= 0)
-      {
+      if (hoursLeft <= 0 || minutesLeft <= 0) {
         let hoursLeft = 0;
         let minutesLeft = 0;
-        console.log('Tirer vos cartes');
+        console.log("Tirer vos cartes");
         console.log(hoursLeft, minutesLeft);
         res.status(200).json({
+          id: userId,
           username: userInfo.username,
           cards: cards_user,
           message: message,
           numberCards: numberCards,
-          remainingTime: hoursLeft + "h " + minutesLeft + "m" ,
+          remainingTime: hoursLeft + "h " + minutesLeft + "m",
         });
       } else {
-        console.log('Vous devez attendre');
+        //console.log("Vous devez attendre");
         res.status(200).json({
+          id: userId,
           username: userInfo.username,
           cards: cards_user,
           message: message,
@@ -87,15 +90,15 @@ export async function getProfil(req, res) {
         });
       }
     } else {
-    res.status(200).json({
-      username: userInfo.username,
-      cards: cards_user,
-      message: message,
-      numberCards: numberCards,
-      remainingTime: '0h 0m',
-    });
-  }
-
+      res.status(200).json({
+        id: userId,
+        username: userInfo.username,
+        cards: cards_user,
+        message: message,
+        numberCards: numberCards,
+        remainingTime: "0h 0m",
+      });
+    }
   } catch (error) {
     console.error(error);
     res.status(500).json({
@@ -110,20 +113,20 @@ export async function getProfilSettings(req, res) {
 
     const userInfo = await prisma.user.findUnique({
       where: {
-        id: userId
-      }, 
+        id: userId,
+      },
       select: {
         username: true,
         email: true,
-        lastDraw: true
-      }
+        lastDraw: true,
+      },
     });
 
     if (!userInfo) {
       // Si l'utilisateur n'a pas d'infos, il n'est pas connecté
       return res.redirect("login");
     }
-    
+
     const lastDraw = userInfo.lastDraw; // Récupère la date du dernier tirage
     const currentTime = new Date().getTime(); // Obtenir le temps actuel et le temps restant jusqu'au prochain tirage
     const timeLeft = lastDraw + 24 * 60 * 60 * 1000 - currentTime; // Ajouter 24 heures en millisecondes pour le prochain tirage
--- frontend/api/getProfile.js
+++ frontend/api/getProfile.js
@@ -7,34 +7,37 @@ fetch("/getprofile")
     // Créer une chaîne de caractères pour le contenu HTML
     let htmlContent = `
       <div class="profile_username">
-
-        <h2>Profil de ${data.username}</h2>
-    </div>
-    <div class="profile_card_container">
+          <h2>Profil de ${data.username}</h2>
+      </div>
+      <div class="profile_card_container">
         <div class="profile_card">
-            <p>${data.username}</p>
+          <p class="profile_card_title">Votre identifiant</p>
+          <div class="profil_card_content">
+              ${data.id}
+          </div>
         </div>
-        <div class="profile_card">
-        <p class="profile_card_title">Nombre de cartes</p>
-        <div class="profil_card_content">
-            ${data.numberCards}
-        </div>
-        </div>
-        <div class="profile_card">
-            <p class="profile_card_title">Prochain tirage</p>
+          <div class="profile_card">
+            <p class="profile_card_title">Nombre de cartes</p>
             <div class="profil_card_content">
-                
-            
-              ${data.remainingTime}  
+                ${data.numberCards}
             </div>
+          </div>
+          <div class="profile_card">
+              <p class="profile_card_title">Prochain tirage</p>
+              <div class="profil_card_content">
+                ${data.remainingTime}
+              </div>
+          </div>
         </div>
-        </div>
-        
         `;
 
-    if (data.remainingTime !== "Tirer vos cartes" && data.remainingTime !== "0h 0m" && data.remainingTime !== "0 h 0 m") {
+    if (
+      data.remainingTime !== "Tirer vos cartes" &&
+      data.remainingTime !== "0h 0m" &&
+      data.remainingTime !== "0 h 0 m"
+    ) {
       htmlContent += `
-        
+
           <div class="draw_button disabled">Tirer vos cartes</div>
         `;
     } else {
@@ -43,7 +46,6 @@ fetch("/getprofile")
           <div class="draw_button">Tirer vos cartes</div>
         </a>`;
     }
-
 
     // Ajouter le contenu HTML à profileContent une seule fois
     profileContent.innerHTML = htmlContent;
@@ -58,10 +60,10 @@ fetch("/getprofile")
                   src="../../img/cartes/${data.cards[i].id_card}.jpg"
                   alt="${data.cards[i].card.name}"
               />
-              
+
           </div>
       `;
-    }  
+    }
   })
   .catch((error) => {
     console.error("Error fetching data:", error);
